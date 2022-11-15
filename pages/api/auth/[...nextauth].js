e4f0a57e85de8e091c94ef20044b7021a74ba96e
vincecoscia <xvincentcosciax@gmail.com>
2022-11-15 09:47:56 -0500

    Added ENV variable for email whitelist

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,7 +15,7 @@ export default NextAuth({
   callbacks: {
     async signIn({ account, profile }) {
       if (account.provider === "google") {
-        return profile.email_verified && profile.email.endsWith("@blackdogadvertising.com")
+        return profile.email_verified && profile.email.endsWith(process.env.EMAIL_WHITELIST)
       }
       return true // Do different verification for other providers that don't have `email_verified`
     },
